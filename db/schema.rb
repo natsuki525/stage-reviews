d0e2b29735e3570f880dcd466d3ec5ac3bea24ab
natsuki525 <eteespoir@icloud.com>
2020-02-05 09:23:37 +0000

    [add] review投稿機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_051849) do
+ActiveRecord::Schema.define(version: 2020_02_05_090241) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,23 @@ ActiveRecord::Schema.define(version: 2020_02_05_051849) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.datetime "stage_date"
+    t.integer "theater_id"
+    t.string "seat"
+    t.integer "view_level"
+    t.integer "satisfaction_level"
+    t.integer "story_level"
+    t.integer "stage_set_level"
+    t.integer "costume_level"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
