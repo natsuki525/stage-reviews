4719a3af00f5622db8817233879cfa9c1246bb52
natsuki525 <eteespoir@icloud.com>
2020-02-08 04:39:53 +0000

    [add] bootstrap4適用

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,9 +2,9 @@ class Review < ApplicationRecord
  belongs_to :user
  attachment :image
 
- enum view_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
- enum satisfaction_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
- enum story_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
- enum stage_set_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
- enum costume_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
+ # enum view_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
+ # enum satisfaction_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
+ # enum story_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
+ # enum stage_set_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
+ # enum costume_level: {★:1, ★★:2, ★★★:3, ★★★★:4, ★★★★★:5}
 end
