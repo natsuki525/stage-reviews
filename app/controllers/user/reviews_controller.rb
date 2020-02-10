eb4bf6e695e3a8c8e6f030478f7ac85388b1d9ae
natsuki525 <eteespoir@icloud.com>
2020-02-10 06:23:52 +0000

    [fix]review/show reviewer表示修正

--- app/controllers/user/reviews_controller.rb
+++ app/controllers/user/reviews_controller.rb
@@ -39,8 +39,8 @@ class User::ReviewsController < ApplicationController
 
  def show
  	@review = Review.find(params[:id])
- 	@user = @review.user_id
- 	@user = User.find(params[:id])
+ 	@user = @review.user
+
  end
 
  def edit
