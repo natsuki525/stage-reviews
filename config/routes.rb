c2fb4e6d91d9e6d50fda828dcaa0035c64162e9e
natsuki525 <eteespoir@icloud.com>
2020-02-11 04:26:54 +0000

    [add]ヘッダー（仮）作成

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,11 @@ Rails.application.routes.draw do
 
   namespace :user do
 
+   resources :users, only: [:show, :edit, :update, :destroy]
+   resource :theater, except: [:show]
    resources :reviews do
      resource :favorites, only: [:create, :destroy]
    end
-   resources :users, only: [:show, :edit, :update, :destroy]
-   resource :theater, except: [:show]
 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
