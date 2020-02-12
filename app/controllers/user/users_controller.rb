cb6f3a369e7fa06c1a4dcac43aad79cbeff97652
natsuki525 <eteespoir@icloud.com>
2020-02-12 03:05:34 +0000

    [fix] デザイン修正

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -5,9 +5,17 @@ class User::UsersController < ApplicationController
  end
 
  def edit
+ 	@user = User.find(params[:id])
  end
 
  def update
+ 	@user = User.find(params[:id])
+ 	if @user.update(user_params)
+ 		redirect_to user_user_path(current_user)
+ 		flash[:notice_update] = "商品が更新されました!"
+ 	else
+ 		render :edit
+ 	end
  end
 
  private
