0e322276fa7a9fc746fcd87e051597a81d412166
natsuki525 <eteespoir@icloud.com>
2020-02-15 10:02:19 +0000

    [add] admin/userコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,10 @@ Rails.application.routes.draw do
    end
 
   end
+
+  namespace :admin do
+    root 'admin/users#index'
+    resources :users, only: [:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
