1ffff2cf65b0e59f82c15a66f5ff804b003f29b1
natsuki525 <eteespoir@icloud.com>
2020-02-18 16:52:51 +0000

    [delete]users/deleted_at削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_151013) do
+ActiveRecord::Schema.define(version: 2020_02_18_165029) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -79,7 +79,6 @@ ActiveRecord::Schema.define(version: 2020_02_18_151013) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_id"
-    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
